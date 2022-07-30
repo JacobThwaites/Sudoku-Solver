682504c03b1701fa66b9338903c3652809be0cff
Jake Thwaites <jakethwaites@hotmail.co.uk>
2022-07-30 13:04:15 +0200

    add clear rows button

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,23 @@ function App() {
     setRows(rows)
   }
 
+  function clearRows() {
+    const newRows = [...rows];
+
+    for (let i = 0; i < newRows.length; i++) {
+      for (let j = 0; j < newRows[i].length; j++) {
+        newRows[i][j] = null;
+      }
+    }
+
+    setRows(newRows);
+  }
+
   return (
     <div>
       <Sudoku handleChange={handleChange} rows={rows}/>
       <SolveButton sudoku={rows} updateRows={updateRows}/>
+      <button onClick={clearRows}>Clear</button>
     </div>
   );
 }
